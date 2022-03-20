b769e5281f237c290c5258c307c9bac85f3d4769
wangdingguo <dgwang97@163.com>
2022-03-20 20:32:48 +0800

    集成apollo配置中心

--- reservationprovider/src/main/java/com/reservationprovider/ReservationproviderApplication.java
+++ reservationprovider/src/main/java/com/reservationprovider/ReservationproviderApplication.java
@@ -1,10 +1,12 @@
 package com.reservationprovider;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableApolloConfig
 @SpringBootApplication
 @EnableDubbo(scanBasePackages = "com.reservationprovider.dubboServiceImpl")
 @MapperScan("com.reservationprovider.dao")
--- reservationprovider/src/main/java/com/reservationprovider/controller/InfoController.java
+++ reservationprovider/src/main/java/com/reservationprovider/controller/InfoController.java
@@ -4,6 +4,7 @@ import com.reservationaip.pojo.User;
 import com.reservationaip.service.IDubboInfoService;
 import com.reservationaip.service.IDubboUserService;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,12 +23,20 @@ public class InfoController {
     /**
      * 在消费者里面 使用stub的指定类,执行存根处理就是在调用真正的方法之前会先执行这个类里面的本方法,一般实现参数的判断什么的
      */
-    @DubboReference(stub = "com.reservationprovider.comment.InfoServiceLocal",mock = "com.reservationprovider.comment.InfoServiceLocal")
+    @DubboReference(stub = "com.reservationprovider.comment.InfoServiceLocal")
     private IDubboInfoService infoService;
+
+    @Value(value = "${test.wdg.name}")
+    private String testName;
 
     @GetMapping("/getUserInfo")
     public User getUserInfo() {
         return infoService.getUserInfo();
+    }
+
+    @GetMapping("/getName")
+    public String getName(){
+        return testName;
     }
 
 }
